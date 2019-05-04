486317dd80efd6c32edbff83788f24a95ce94431
chn-liuxin <50198598+chn-liuxin@users.noreply.github.com>
2019-05-04 10:09:47 +0800

    Create SubjectServiceImpl.java
    获取课程表简单Demo实现

---
+++ src/main/java/edu/cuit/autumn/service/impl/SubjectServiceImpl.java
@@ -0,0 +1,46 @@
+package edu.cuit.autumn.service.impl;
+
+import edu.cuit.autumn.entity.Subject;
+import edu.cuit.autumn.mapper.SubjectMapper;
+import edu.cuit.autumn.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubjectServiceImpl implements SubjectService {
+
+    @Autowired
+    SubjectMapper subjectMapper;
+
+    @Override
+    public List<Subject> getAllSubject() {
+        return subjectMapper.getAllSubject();
+    }
+
+    @Override
+    public List<Subject> getSubjectByType(String subjectType) {
+        return subjectMapper.getSubjectByType(subjectType);
+    }
+
+    @Override
+    public void insertSubject(Subject subject) {
+        subjectMapper.insertSubject(subject);
+    }
+
+    @Override
+    public void deleteSubject(Subject subject) {
+        subjectMapper.deleteSubject(subject);
+    }
+
+    @Override
+    public void updateSubject(Subject subject) {
+        subjectMapper.updateSubject(subject);
+    }
+
+    @Override
+    public Subject getSubjectById(String subjectId) {
+        return subjectMapper.getSubjectById(subjectId);
+    }
+}
